0e0709c4438a40e4c0ff338d24ec0d1efe81987a
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-04 16:28:14 +0800

    Create Step_2_T_cell_extraction.R

---
+++ Section_1_basic_analysis/Step_2_T_cell_extraction.R
@@ -0,0 +1,9 @@
+#################################
+### Section 1, Basic analysis ###
+#################################
+
+################################################################################
+### T cell extraction, cell composition and differential expression analysis ###
+################################################################################
+
+### T cell extraction from pre-processed seurat object
