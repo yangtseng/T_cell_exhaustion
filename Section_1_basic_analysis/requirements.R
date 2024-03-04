b9efed7661dc69cd33da8c0548d14533ec76be68
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-04 16:18:22 +0800

    Create requirements.R

---
+++ Section_1_basic_analysis/requirements.R
@@ -0,0 +1,25 @@
+###########################################
+### Profiles that are used for analysis ###
+###########################################
+
+
+
+############################################
+### Packages that are used in this study ###
+############################################
+
+### single cell analysis
+library(Seurat)
+library(scater)
+library(Matrix)
+library(igraph)
+library(SCENIC)
+library(circlize)
+library(dplyr)
+library(readr)
+library(Doubletfinder)
+
+### ploting
+library(ggplot2)
+library(ggpubr)
+library(ggtext)
