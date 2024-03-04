02310516192b2b08b4a882e0cd77ac24b0732d19
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-04 09:16:50 +0800

    Create step1_Preprocessing.R

---
+++ Section_1_basic_analysis/step1_Preprocessing.R
@@ -0,0 +1,15 @@
+#################################
+### Section 1, Basic analyssi ###
+#################################
+
+###############################################
+### Data preprocessing and doublets removal ###
+###############################################
+
+### This is the section of murine model analysis which can be downloaded in raw and processed forms from the NCBI Gene Expression Omnibus under accession number [NUMBER]
+### If you are looking for the cell-line like model analysis, please refer to the section 3
+
+###############################################################
+### Step 1, read the raw files and remove low quality cells ###
+###############################################################
+
