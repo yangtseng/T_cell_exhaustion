277114c822647ba8af2cb92bdeff3c5931456eac
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-05 10:54:05 +0800

    Update Step_5_Tex_progenitor.R

--- Section_1_basic_analysis/Step_5_Tex_progenitor.R
+++ Section_1_basic_analysis/Step_5_Tex_progenitor.R
@@ -1,1 +1,18 @@
+#################################
+### Section 1, Basic analysis ###
+#################################
 
+#######################################################
+### Further analysis of Tex progenitor in cluster 4 ###
+#######################################################
+
+set.seed(1234)
+work_path = "./"
+source("requirements.R")
+
+load("murine_tcell3.rds")
+### It will load a pre-processed seurat object of T cells from section 1, step 3
+
+################################
+### Subset of Tex progenitor ###
+################################
