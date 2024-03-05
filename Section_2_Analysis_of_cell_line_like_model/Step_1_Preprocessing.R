202dd56752141cf60c8e90fc69a3d511ad3860f9
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-05 13:48:35 +0800

    Update Step_1_Preprocessing.R

--- Section_2_Analysis_of_cell_line_like_model/Step_1_Preprocessing.R
+++ Section_2_Analysis_of_cell_line_like_model/Step_1_Preprocessing.R
@@ -93,33 +93,6 @@ multiome_preprocessing <- function(sample_path, project){
     annotation = annotation
   )
 
-  ### DNA accessibility data processing 
-  DefaultAssay(tcell) <- "peaks"
-  tcell <- FindTopFeatures(tcell, min.cutoff = 5)
-  tcell <- RunTFIDF(tcell)
-  tcell <- RunSVD(tcell)
-
-  ############################################
-  ### Data preprocessing of scRNA-seq data ###
-  ############################################
-
-  ### Set default assay to RNA
-  DefaultAssay(tcell) <- "RNA"
-  
-  ### We normalized the gene expression through sctransform
-  tcell <- SCTransform(tcell)  
-
-  ### Dimensional reduction based on scRNA-seq data only
-  tcell <- RunPCA(tcell)
-
-  ### Lastly, we built a joint UMA visualization
-  tcell <- RunUMAP(
-    object = tcell,
-    nn.name = "weighted.nn",
-    assay = "RNA",
-    verbose = TRUE
-  )
-
   return(tcell)
 }
 
