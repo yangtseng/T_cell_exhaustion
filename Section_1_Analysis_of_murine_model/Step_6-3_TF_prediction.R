e36d1ab3d6f237453400457add9e5082b4900016
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-05 15:07:15 +0800

    Update Step_6-3_TF_prediction.R

--- Section_1_Analysis_of_murine_model/Step_6-3_TF_prediction.R
+++ Section_1_Analysis_of_murine_model/Step_6-3_TF_prediction.R
@@ -12,3 +12,11 @@ source("requirements.R")
 
 ### We mainly followed the tutorial of SCENIC 
 ### http://htmlpreview.github.io/?https://github.com/aertslab/SCENIC/blob/master/inst/doc/SCENIC_Running.html
+
+######################################
+### Step 1, Import GRNboost output ###
+######################################
+
+GRNBoost_output <- read.delim(paste0(work_path, "int/1.1_grn_output.tsv"), header=FALSE)
+colnames(GRNBoost_output) <- c("TF","Target","weight")
+saveRDS(GRNBoost_output, file="int/1.4_GENIE3_linkList.Rds")
