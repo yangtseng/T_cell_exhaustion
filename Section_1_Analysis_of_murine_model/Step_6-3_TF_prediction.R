ed90e01f42ebb8b9e96b6fa95210cfd248dbe79c
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-05 15:05:37 +0800

    Create Step_6-3_TF_prediction.R

---
+++ Section_1_Analysis_of_murine_model/Step_6-3_TF_prediction.R
@@ -0,0 +1,14 @@
+###########################################
+### Section 1, Anlaysis of murine model ###
+###########################################
+
+#######################################
+### Transcription factor prediction ###
+#######################################
+
+set.seed(1234)
+work_path = "./"
+source("requirements.R")
+
+### We mainly followed the tutorial of SCENIC 
+### http://htmlpreview.github.io/?https://github.com/aertslab/SCENIC/blob/master/inst/doc/SCENIC_Running.html
