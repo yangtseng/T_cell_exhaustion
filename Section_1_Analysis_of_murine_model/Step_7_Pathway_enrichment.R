bc01dca27d549c8bf7ec6f15b41a57ca88a3e58a
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-05 16:04:02 +0800

    Update Step_7_Pathway_enrichment.R

--- Section_1_Analysis_of_murine_model/Step_7_Pathway_enrichment.R
+++ Section_1_Analysis_of_murine_model/Step_7_Pathway_enrichment.R
@@ -1,1 +1,13 @@
+###########################################
+### Section 1, Anlaysis of murine model ###
+###########################################
 
+#######################################
+### Transcription factor prediction ###
+#######################################
+
+set.seed(1234)
+work_path = "./"
+source("requirements.R")
+
+HCC.tcell <- readRDS(paste0(work_path, "murine_tcell_TF6.rds"))
