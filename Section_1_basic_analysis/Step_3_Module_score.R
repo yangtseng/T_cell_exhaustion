2637c25a8a78baf60e799e16a1f4424432e0f9d1
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-05 09:26:36 +0800

    Update Step_3_Module_score.R

--- Section_1_basic_analysis/Step_3_Module_score.R
+++ Section_1_basic_analysis/Step_3_Module_score.R
@@ -1,1 +1,47 @@
+#################################
+### Section 1, Basic analysis ###
+#################################
 
+################################################################
+### Step 3, Calculation of module score and cell cycle score ###
+################################################################
+
+set.seed(1234)
+work_path = "./"
+source("requirements.R")
+
+load("murine_tcell3.rds")
+### It will load a pre-processed seurat object of T cells from section 1, step 2
+
+################################
+### Module score calculation ###
+################################
+
+### We calcuated 3 different module scores including effectory, exhaustion and immune checkpoint modules
+### The gene sets were collected from other related articles
+
+### T cell effectory gene set
+eff <- list(c('Ifng','Ccl3','Ccl4','Prf1','Nkg7','Gzmb','Gzmk'))
+HCC.tcell <- AddModuleScore(HCC.tcell, features = eff, name = 'effectory')
+
+### T cell exhaustion gene set
+exhaust <- list(c('Pdcd1','Cd244a', 'Lag3','Tigit','Eomes','Tox'))
+HCC.tcell <- AddModuleScore(HCC.tcell, features = exhaust, name = 'exhaustion')
+
+### We calculated the module score of each cell via AddModuleScore() in seurat
+cp <-  list(c('Havcr2', 'Tigit', 'Ctla4', 'Pdcd1', 'Lag3', 'Entpd1'))
+HCC.tcell <- AddModuleScore(HCC.tcell, features = cp, name = 'checkpoint')
+
+####################################
+### Cell cycle score calculation ###
+####################################
+
+### We calcuated the cell cycle score following the seurat tutorial
+### The gene sets were collected from Tirosh et al, 2015 which could be loaded with Seurat directly
+s.genes <- cc.genes$s.genes
+g2m.genes <- cc.genes$g2m.genes
+
+### Cell cycle score calculation via CellCycleScoring()
+HCC.tcell <- CellCycleScoring(HCC.tcell, s.features = s.genes, g2m.features = g2m.genes, set.ident = TRUE)
+
+### If you would like to check out the code for the visualization of main figures, please refer to section 4.
