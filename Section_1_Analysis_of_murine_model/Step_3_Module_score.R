61f8655fe5369188f948d7b9bd8e4b90f0f8d9e5
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-05 14:55:38 +0800

    Update Step_3_Module_score.R

--- Section_1_Analysis_of_murine_model/Step_3_Module_score.R
+++ Section_1_Analysis_of_murine_model/Step_3_Module_score.R
@@ -10,7 +10,7 @@ set.seed(1234)
 work_path = "./"
 source("requirements.R")
 
-load("murine_tcell3.rds")
+HCC.tcell <- readRDS(paste0(work_path,"murine_tcell3.rds"))
 ### It will load a pre-processed seurat object of T cells from section 1, step 2
 
 ################################
