1a882ecf02e898c39a0a81b47f667d41034fd2e8
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-05 09:55:30 +0800

    Update Step_4_scVelo.R

--- Section_1_basic_analysis/Step_4_scVelo.R
+++ Section_1_basic_analysis/Step_4_scVelo.R
@@ -19,4 +19,12 @@ load("murine_tcell_modulescore4.rds")
 ### Step 1, write H5Seurat file ###
 ###################################
 
-SaveH5Seurat(HCC.tcell, '~/Desktop/HCC_tcell.H5Seurat', overwrite = TRUE)
+SaveH5Seurat(HCC.tcell, paste0(workt_path, "HCC_tcell.H5Seurat"), overwrite = TRUE)
+
+###############################################
+### Step 2, convert H5Seurat file into h5ad ###
+###############################################
+
+Convert(paste0(work_path, "HCC_tcell.h5Seurat"), dest = "h5ad")
+
+### Please refer to Step_4_scVelo.ipynb for later analysis
