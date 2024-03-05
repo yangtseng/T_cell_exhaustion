30b30f4717278db1f0a0dc4371f8ded534dbda5f
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-05 14:46:58 +0800

    Update Step_6_TF_prediction.R

--- Section_1_Analysis_of_murine_model/Step_6_TF_prediction.R
+++ Section_1_Analysis_of_murine_model/Step_6_TF_prediction.R
@@ -18,6 +18,7 @@ load("murine_tcell_modulescore4.rds")
 #############################################
 ### Step 1, Initialize settings of SCENIC ###
 #############################################
+
 org <- "mgi" # or hgnc, or dmel
 dbDir <- paste0(work_path, "cisTarget-databases") # RcisTarget databases location
 myDatasetTitle <- "TF_in_murine_model" # choose a name for your analysis
@@ -32,8 +33,30 @@ saveRDS(scenicOptions, file=paste0(work_path, "int/scenicOptions.Rds"))
 ###################################################
 
 ### Raw counts from seurat object
-exprMat <- as.matrix(HCC_tcell@assays[["RNA"]]@counts)
-cellInfo <- data.frame(seuratClusters=Idents(HCC_tcell))
-scenicOptions@inputDatasetInfo$cellInfo <- cellInfo
-clusterInfo <- clusterInfo@cell.embeddings
+exprMat <- as.matrix(HCC.tcell@assays[["RNA"]]@counts)
 
+### Cell information
+cellInfo <- data.frame(seuratClusters=Idents(HCC.tcell))
+scenicOptions@inputDatasetInfo$cellInfo <- cellInfo
+
+### Extract cluster information from seurat object
+clusterInfo <- HCC.tcell@reductions[["umap"]]@cell.embeddings
+
+###################################################
+### Step 3, Construct the co-expression network ###
+###################################################
+
+### Genes overlap with RcisTarget
+genesKept <- geneFiltering(exprMat, scenicOptions)
+
+### There were 6905 genes available in RcisTarget
+exprMat_filtered <- exprMat[genesKept, ]
+
+### Construct the co-expression network
+runCorrelation(exprMat_filtered, scenicOptions)
+
+### log normalization
+exprMat_filtered_log <- log2(exprMat_filtered + 1)
+### Export the data to use GRNboost in python
+exportsForArboreto(exprMat_filtered_log, scenicOptions, dir = "int")
+### We conducted the GRNboost on python, please refer to Section_1_Analysis_of_murine_model/Step_6_GRNboost.py
