fa9760e870dc3c843a8a6df2a377734ce016217e
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-05 13:48:56 +0800

    Update Step_2_Integration.R

--- Section_2_Analysis_of_cell_line_like_model/Step_2_Integration.R
+++ Section_2_Analysis_of_cell_line_like_model/Step_2_Integration.R
@@ -1,1 +1,20 @@
+############################################
+### Data preprocessing of scRNA-seq data ###
+############################################
 
+### Set default assay to RNA
+DefaultAssay(tcell) <- "RNA"
+
+### We normalized the gene expression through sctransform
+tcell <- SCTransform(tcell)  
+
+### Dimensional reduction based on scRNA-seq data only
+tcell <- RunPCA(tcell)
+
+### Lastly, we built a joint UMA visualization
+tcell <- RunUMAP(
+  object = tcell,
+  nn.name = "weighted.nn",
+  assay = "RNA",
+  verbose = TRUE
+)
