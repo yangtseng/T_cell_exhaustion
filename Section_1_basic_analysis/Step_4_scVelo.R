90d57365ac5cd34d54c2e1a45ead9df50e6b05d6
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-05 09:46:07 +0800

    Update Step_4_scVelo.R

--- Section_1_basic_analysis/Step_4_scVelo.R
+++ Section_1_basic_analysis/Step_4_scVelo.R
@@ -1,1 +1,16 @@
+#################################
+### Section 1, Basic analysis ###
+#################################
 
+###############################
+### RNA velocity via scVelo ###
+###############################
+
+### The scVelo is running under python 
+### Here, we converted original Seurat object into h5ad for scanpy and scVelo in python
+
+###################################
+### Step 1, write H5Seurat file ###
+###################################
+
+SaveH5Seurat(HCC.tcell, '~/Desktop/HCC_tcell.H5Seurat', overwrite = TRUE)
