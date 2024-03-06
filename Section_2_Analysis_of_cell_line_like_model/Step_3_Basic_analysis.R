9f745dc3c745197e90dfd1faefaab02204216fd4
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-06 15:12:45 +0800

    Update and rename Step_3_Module_score_and_gene_activity.R to Step_3_Basic_analysis.R

---
+++ Section_2_Analysis_of_cell_line_like_model/Step_3_Basic_analysis.R
@@ -0,0 +1,66 @@
+###################################################
+### Section 2, Anlaysis of cell line like model ###
+###################################################
+
+########################################################################
+### Module score, gene activity and differential expression analysis ###
+########################################################################
+
+set.seed(1234)
+
+work_path = "./"
+source(paste0(work_path, "requirements.R"))
+
+### Here, we loaded the integrated multiome seurat object
+tcell <- readRDS("Cellline_multiome1.rds")
+
+##########################################
+### Step 1, Calculate the module score ###
+##########################################
+
+### Set default assay as SCT
+DefaultAssay(tcell) <- "SCT"
+
+### Calculate the module score of exhausted and effectory gene set
+
+### T cell effectory gene set
+eff <- list(c('Ifng','Ccl3','Ccl4','Prf1','Nkg7','Gzmb','Gzmk'))
+tcell <- AddModuleScore(tcell, features = eff, name = 'effectory')
+
+### T cell exhaustion gene set
+exhaust <- list(c('Pdcd1','Cd244a', 'Lag3','Tigit','Eomes','Tox'))
+tcell <- AddModuleScore(tcell, features = exhaust, name = 'exhaustion')
+
+###########################################
+### Step 2, Calculate the gene activity ###
+###########################################
+
+### Set default assay as peaks 
+DefaultAssay(tcell) <- "peaks"
+
+### Gene activity calculation
+gene.activities <- GeneActivity(tcell)
+
+### Create a new assay for gene activity storage
+tcell[["GA"]] <- CreateAssayObject(counts = gene.activities)
+
+### Normalization and scaling
+DefaultAssay(exvivo240202) <- "GA"
+tcell <- NormalizeData(tcell)
+tcell <- ScaleData(tcell)
+
+################################################
+### Step 3, Differential expression analysis ###
+################################################
+
+### Identifying the differentially expressed genes (DEGs)
+tcell_DEGs <- FindAllMarkers(tcell, assay = 'SCT', logfc.threshold = 0.4, only.pos = T)
+
+### Identifying the differentially activated genes (based on gene activity)
+tcell_DAGs <- FindAllMarkers(tcell, assay = 'GA', logfc.threshold = 0.3, only.pos = T)
+
+### save files
+write.csv(tcell_DEGs, paste0(work_path, "cell_line_like_DEGs.csv"))
+write.csv(tcell_DAGs, paste0(work_path, "cell_line_like_DAGs.csv"))
+
+saveRDS(tcell, paste0(work_path, "Cellline_multiome2.rds")
