851df368fbdb5227eb119689416a520115383437
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-06 16:57:03 +0800

    Update Step_4-3_TF_prediction.R

--- Section_2_Analysis_of_cell_line_like_model/Step_4-3_TF_prediction.R
+++ Section_2_Analysis_of_cell_line_like_model/Step_4-3_TF_prediction.R
@@ -17,3 +17,24 @@ source("requirements.R")
 scenicOptions <- readRDS(paste0(work_path, "int/scenicOptions.rds"))
 exprMat_filtered_log <- readRDS(paste0(work_path, "int/exprMat_filtered_log.rds"))
 
+
+######################################
+### Step 1, Import GRNboost output ###
+######################################
+
+GRNBoost_output <- read.delim(paste0(work_path, "int/1.1_grn_output.tsv"), header=FALSE)
+colnames(GRNBoost_output) <- c("TF","Target","weight")
+saveRDS(GRNBoost_output, file=paste0(work_path, "int/1.4_GRNboost_linkList.rds"))
+
+#########################
+### Step 2, RunSCENIC ###
+#########################
+
+### runSCENIC setting
+scenicOptions@settings$verbose <- TRUE
+scenicOptions@settings$nCores <- 10
+scenicOptions@settings$seed <- 123
+
+scenicOptions <- runSCENIC_1_coexNetwork2modules(scenicOptions)
+scenicOptions <- runSCENIC_2_createRegulons(scenicOptions)
+scenicOptions <- runSCENIC_3_scoreCells(scenicOptions, exprMat_filtered_log)
