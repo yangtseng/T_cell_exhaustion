cb4e99701819da9f932238d3dc029659079cd5d9
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-06 14:47:46 +0800

    Create Step_3_Module_score_and_gene_activity.R

---
+++ Section_2_Analysis_of_cell_line_like_model/Step_3_Module_score_and_gene_activity.R
@@ -0,0 +1,14 @@
+###################################################
+### Section 2, Anlaysis of cell line like model ###
+###################################################
+
+######################################
+### Module score and gene activity ###
+######################################
+
+set.seed(1234)
+
+work_path = "./"
+source(paste0(work_path, "requirements.R"))
+
+### Here, we loaded the integrated multiome seurat object
