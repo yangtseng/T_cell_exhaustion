38f094ff2991ebf125d5eb5c2b84967da95d90ef
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-06 15:37:58 +0800

    Update Step_4_SCENIC_initialization.R

--- Section_2_Analysis_of_cell_line_like_model/Step_4_SCENIC_initialization.R
+++ Section_2_Analysis_of_cell_line_like_model/Step_4_SCENIC_initialization.R
@@ -7,7 +7,7 @@
 #######################################
 
 set.seed(1234)
-work_path = "./"
+work_path = "./Cellline/"
 source("requirements.R")
 
 tcell <- readRDS(paste0(work_path, "Cellline_multiome2.rds"))
@@ -23,4 +23,26 @@ org <- "mgi" # or hgnc, or dmel
 dbDir <- paste0(work_path, "cisTarget-databases") # RcisTarget databases location
 myDatasetTitle <- "TF_in_cellline_like_model" # choose a name for your analysis
 
-###
+dbs <- defaultDbNames[[org]]
+scenicOptions <- initializeScenic(org=org, dbDir=dbDir, dbs=dbs, datasetTitle=myDatasetTitle, nCores=12)
+### save the initialize settings
+saveRDS(scenicOptions, file=paste0(work_path, "int/scenicOptions.Rds"))
+
+###################################################
+### Step 2, Extraction of raw expression matrix ###
+###################################################
+
+### Raw counts from seurat object
+exprMat <- as.matrix(tcell@assays[["RNA"]]@counts)
+
+### Cell information
+cellInfo <- data.frame(seuratClusters=Idents(tcell))
+scenicOptions@inputDatasetInfo$cellInfo <- cellInfo
+
+### Extract cluster information from seurat object
+clusterInfo <- tcell@reductions[["umap"]]@cell.embeddings
+
+###################################################
+### Step 3, Construct the co-expression network ###
+###################################################
+
