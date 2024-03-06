1953b3e5c5d6c975cd34977bdb0852d76a29f366
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-06 15:03:32 +0800

    Update Step_3_Module_score_and_gene_activity.R

--- Section_2_Analysis_of_cell_line_like_model/Step_3_Module_score_and_gene_activity.R
+++ Section_2_Analysis_of_cell_line_like_model/Step_3_Module_score_and_gene_activity.R
@@ -12,3 +12,14 @@ work_path = "./"
 source(paste0(work_path, "requirements.R"))
 
 ### Here, we loaded the integrated multiome seurat object
+tcell <- readRDS("Cellline_multiome1.rds")
+
+##########################################
+### Step 1, Calculate the module score ###
+##########################################
+
+### Set default assay as SCT
+DefaultAssay(tcell) <- "SCT"
+
+### Calculate the module score of exhausted and effectory gene set
+
