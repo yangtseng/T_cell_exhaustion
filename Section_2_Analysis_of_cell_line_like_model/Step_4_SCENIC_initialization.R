cdd32e570dd42b5068fa117dae9cd83c42bbb336
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-06 15:34:20 +0800

    Update Step_4_SCENIC_initialization.R

--- Section_2_Analysis_of_cell_line_like_model/Step_4_SCENIC_initialization.R
+++ Section_2_Analysis_of_cell_line_like_model/Step_4_SCENIC_initialization.R
@@ -1,1 +1,24 @@
+###########################################
+### Section 1, Anlaysis of murine model ###
+###########################################
 
+#######################################
+### Transcription factor prediction ###
+#######################################
+
+set.seed(1234)
+work_path = "./"
+source("requirements.R")
+
+HCC.tcell <- readRDS(paste0(work_path, "murine_tcell_modulescore4.rds"))
+
+### We mainly followed the tutorial of SCENIC 
+### http://htmlpreview.github.io/?https://github.com/aertslab/SCENIC/blob/master/inst/doc/SCENIC_Running.html
+
+#############################################
+### Step 1, Initialize settings of SCENIC ###
+#############################################
+
+org <- "mgi" # or hgnc, or dmel
+dbDir <- paste0(work_path, "cisTarget-databases") # RcisTarget databases location
+myDatasetTitle <- "TF_in_murine_model" # choose a name for your analysis
