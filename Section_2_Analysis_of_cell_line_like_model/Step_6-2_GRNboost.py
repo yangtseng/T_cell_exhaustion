4935e766abc6a68edd711ac186c2c6e6527f9b2c
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-06 16:53:52 +0800

    Add files via upload

---
+++ Section_2_Analysis_of_cell_line_like_model/Step_6-2_GRNboost.py
@@ -0,0 +1,40 @@
+###########################################
+### Section 1, Anlaysis of murine model ###
+###########################################
+
+#######################################
+### Transcription factor prediction ###
+#######################################
+
+################
+### GRNboost ###
+################
+
+### packages
+import pandas as pd
+
+from distributed import Client, LocalCluster
+from arboreto.utils import load_tf_names
+from arboreto.algo import grnboost2
+
+in_file  = 'int/1.1_exprMatrix_filtered_t.txt'
+tf_file  = 'int/1.1_inputTFs.txt'
+out_file = 'int/1.1_grn_output.tsv'
+
+### ex_matrix wass a DataFrame with gene names as column names
+ex_matrix = pd.read_csv(in_file, sep='\t')
+
+### tf_names was read using a utility function included in Arboreto
+tf_names = load_tf_names(tf_file)
+
+### We instantiated a custom Dask distributed Client
+client = Client(LocalCluster())
+
+### We computed the GRN
+network = grnboost2(expression_data=ex_matrix,
+                    tf_names=tf_names,
+                    client_or_address=client)
+
+### We saved the GRN to file
+network.to_csv(out_file, sep='\t', index=False, header=False)
+### We head to Step_6-3_TF_prediction.R
