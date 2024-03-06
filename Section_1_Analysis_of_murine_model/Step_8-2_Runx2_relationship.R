2e0d55986c2f7a16b90f69a0e7f6b500f8f31ffa
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-06 13:33:35 +0800

    Update Step_8-2_Runx2_relationship.R

--- Section_1_Analysis_of_murine_model/Step_8-2_Runx2_relationship.R
+++ Section_1_Analysis_of_murine_model/Step_8-2_Runx2_relationship.R
@@ -1,1 +1,13 @@
+###########################################
+### Section 1, Anlaysis of murine model ###
+###########################################
 
+############################################################
+### Runx2 relationship with markers of T cell exhaustion ###
+############################################################
+
+set.seed(1234)
+work_path = "./"
+source("requirements.R")
+
+HCC.tcell <- readRDS(paste0(work_path, "murine_tcell_Pathway7.rds"))
