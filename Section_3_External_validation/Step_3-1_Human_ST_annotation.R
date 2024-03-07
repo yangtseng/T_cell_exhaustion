e50b5b6fafb461d779ab4b856d2b797f76af57c1
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-07 15:38:50 +0800

    Update Step_3-1_Human_ST_annotation.R

--- Section_3_External_validation/Step_3-1_Human_ST_annotation.R
+++ Section_3_External_validation/Step_3-1_Human_ST_annotation.R
@@ -10,3 +10,16 @@ set.seed(1234)
 work_path = "./external_validation/"
 source("requirements.R")
 
+##############################
+### Step 1, Basic analysis ###
+##############################
+
+### Load ST data
+P1T <- readRDS(paste0(work_path, "Human_ST/P1T_Spatial.rds.gz"))
+P3T <- readRDS(paste0(work_path, "Human_ST/P3T_Spatial.rds.gz"))
+P5T <- readRDS(paste0(work_path, "Human_ST/P5T_Spatial.rds.gz"))
+P7T <- readRDS(paste0(work_path, "Human_ST/P7T_Spatial.rds.gz"))
+P8T <- readRDS(paste0(work_path, "Human_ST/P8T_Spatial.rds.gz"))
+P9T <- readRDS(paste0(work_path, "Human_ST/P9T_Spatial.rds.gz"))
+P10T <- readRDS(paste0(work_path, "Human_ST/P10T_Spatial.rds.gz"))
+P11T <- readRDS(paste0(work_path, "Human_ST/P11T_Spatial.rds.gz"))
