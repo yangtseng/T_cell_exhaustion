e468d8c2483abcd26a995876122b1cbfd7d94b94
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-07 16:28:38 +0800

    Update Step_3-1_Human_ST_annotation.R

--- Section_3_External_validation/Step_3-1_Human_ST_annotation.R
+++ Section_3_External_validation/Step_3-1_Human_ST_annotation.R
@@ -10,9 +10,6 @@ set.seed(1234)
 work_path = "./external_validation/"
 source("requirements.R")
 
-##############################
-### Step 1, Basic analysis ###
-##############################
 
 ### Load ST data
 P1T <- readRDS(paste0(work_path, "Human_ST/P1T_Spatial.rds.gz"))
@@ -24,7 +21,10 @@ P9T <- readRDS(paste0(work_path, "Human_ST/P9T_Spatial.rds.gz"))
 P10T <- readRDS(paste0(work_path, "Human_ST/P10T_Spatial.rds.gz"))
 P11T <- readRDS(paste0(work_path, "Human_ST/P11T_Spatial.rds.gz"))
 
-### Data preprocessing
+##################################
+### Step 1, Data preprocessing ###
+##################################
+
 SCT_preprocessing <- function(data, dim, verbose){
   data <- SCTransform(data, assay = 'Spatial', verbose = verbose, min_cells = 3)
   data <- RunPCA(data, assay = 'SCT', verbose = verbose)
