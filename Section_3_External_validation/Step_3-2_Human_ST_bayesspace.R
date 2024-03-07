63215be02300831482ceee5b0d007ad4c7913d76
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-07 15:51:06 +0800

    Update Step_3-2_Human_ST_bayesspace.R

--- Section_3_External_validation/Step_3-2_Human_ST_bayesspace.R
+++ Section_3_External_validation/Step_3-2_Human_ST_bayesspace.R
@@ -1,1 +1,18 @@
+###################################################################
+### Section 3, External validation of Runx2 in exhausted T cell ###
+###################################################################
 
+##############################################################
+### Validation using human spatial transcriptomics dataset ###
+##############################################################
+
+set.seed(1234)
+work_path = "./external_validation/"
+source("requirements.R")
+
+###################################
+### Step 1, Spatial enhancement ###
+###################################
+
+### Load ST data
+load(paste(work_path, "Human_ST_annotated.RData"))
