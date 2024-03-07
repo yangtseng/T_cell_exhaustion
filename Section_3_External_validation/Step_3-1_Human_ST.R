2f33073a8eb5596416d90c29d33b071d49ea18df
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-07 14:38:03 +0800

    Create Step_3-1_Human_ST.R

---
+++ Section_3_External_validation/Step_3-1_Human_ST.R
@@ -0,0 +1,11 @@
+###################################################################
+### Section 3, External validation of Runx2 in exhausted T cell ###
+###################################################################
+
+##############################################################
+### Validation using human spatial transcriptomics dataset ###
+##############################################################
+
+set.seed(1234)
+work_path = "./external_validation/"
+source("requirements.R")
