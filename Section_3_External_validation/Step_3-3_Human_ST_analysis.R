ce5f083bd13e486b78f482c07728bd9a1291acc4
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-07 16:31:33 +0800

    Update Step_3-3_Human_ST_analysis.R

--- Section_3_External_validation/Step_3-3_Human_ST_analysis.R
+++ Section_3_External_validation/Step_3-3_Human_ST_analysis.R
@@ -1,1 +1,34 @@
+###################################################################
+### Section 3, External validation of Runx2 in exhausted T cell ###
+###################################################################
 
+##############################################################
+### Validation using human spatial transcriptomics dataset ###
+##############################################################
+
+set.seed(1234)
+work_path = "./external_validation/"
+source("requirements.R")
+
+Load(paste0(work_path, "Human_ST_bayesspace.RData"))
+
+#############################
+### Step 1, Normalization ###
+#############################
+
+### Before analyzing the enhanced ST data, we first normalized the assay
+Enh_norm <- function(data){
+  DefaultAssay(data) <- "Enhanced"
+  data <- NormalizeData(data)
+  data <- ScaleData(data)
+  return(data)
+}
+
+P1T <- Enh_norm(P1T)
+P3T <- Enh_norm(P3T)
+P5T <- Enh_norm(P5T)
+P7T <- Enh_norm(P7T)
+P8T <- Enh_norm(P8T)
+P9T <- Enh_norm(P9T)
+P10T <- Enh_norm(P10T)
+P11T <- Enh_norm(P11T)
