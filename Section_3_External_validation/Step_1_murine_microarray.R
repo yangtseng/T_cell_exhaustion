cfee574115f22f02f55809024bdec257175a84d0
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-07 10:43:01 +0800

    Update Step_1_murine_microarray.R

--- Section_3_External_validation/Step_1_murine_microarray.R
+++ Section_3_External_validation/Step_1_murine_microarray.R
@@ -16,6 +16,9 @@ source("requirements.R")
 
 ### Load microarray data
 murine_data <- ReadAffy(celfile.path = paste0(work_path, "murine_data"))
+### The data was from the article: Cabozantinib enhances anti-PD1 activity and elicits a neutrophil-based immune response in hepatocellular carcinoma
+### https://www.ncbi.nlm.nih.gov/pmc/articles/PMC9167725/
+### GEO accession number: GSE174770 [https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE174770]
 
 ### Preprocessing of microarray data
 eset.rma <- rma(murine_data)
