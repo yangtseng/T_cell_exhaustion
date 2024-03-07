b368bde1dbe6e461c908b074d84c607a2a835f18
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-07 09:07:44 +0800

    Update Step_1_murine_microarray.R

--- Section_3_External_validation/Step_1_murine_microarray.R
+++ Section_3_External_validation/Step_1_murine_microarray.R
@@ -1,1 +1,14 @@
+###################################################################
+### Section 3, External validation of Runx2 in exhausted T cell ###
+###################################################################
+
+##################################################
+### Validation using murine microarray dataset ###
+##################################################
+
+set.seed(1234)
+work_path = "./external_validation/"
+source("requirements.R")
+
+### Load microarray data
 
