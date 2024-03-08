5f67a4e57265d1a1b2a4c24f2c5b7aad81f9c813
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-08 13:49:55 +0800

    Create Fig_2a.R

---
+++ Section_4_Figure_code/Fig_2a.R
@@ -0,0 +1,16 @@
+########################################
+### Section 4, Code for main figures ###
+########################################
+
+#################
+### Figure 2a ###
+#################
+
+set.seed(1234)
+work_path = "./main_figures/"
+source("requirements.R")
+
+### Load latest file
+tcell <- readRDS("./murine_tcell_Runx28.rds")
+
+### Please refer to Section_1 Step_3_Module_score for related analysis code
