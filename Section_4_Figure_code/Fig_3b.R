9e3cceec4cb3be335ee5511d4de42c1921d0e4c5
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-08 15:47:56 +0800

    Update Fig_3b.R

--- Section_4_Figure_code/Fig_3b.R
+++ Section_4_Figure_code/Fig_3b.R
@@ -1,1 +1,14 @@
+########################################
+### Section 4, Code for main figures ###
+########################################
 
+#################
+### Figure 3b ###
+#################
+
+set.seed(1234)
+work_path = "./main_figures/"
+source("requirements.R")
+
+### Load latest file
+HCC.tcell <- readRDS("./murine_tcell_Runx28.rds")
