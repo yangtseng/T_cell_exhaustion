b62a02a005c3a43784efdc15e393a1b718e43fb0
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-08 17:19:52 +0800

    Update Fig_5a.R

--- Section_4_Figure_code/Fig_5a.R
+++ Section_4_Figure_code/Fig_5a.R
@@ -1,1 +1,14 @@
-murine_microarray.RData
+########################################
+### Section 4, Code for main figures ###
+########################################
+
+#################
+### Figure 4d ###
+#################
+
+set.seed(1234)
+work_path = "./main_figures/"
+source("requirements.R")
+
+### Load latest file
+load("./external_validation/murine_microarray.RData")
