24fa107f4dea0df262fb1a5337fee30b6a26ac10
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-08 16:23:08 +0800

    Update Fig_4b.R

--- Section_4_Figure_code/Fig_4b.R
+++ Section_4_Figure_code/Fig_4b.R
@@ -12,3 +12,17 @@ source("requirements.R")
 
 ### Load latest file
 tcell <- readRDS("./Cellline/Cellline_motif4.rds")
+
+### Set default assay to chromVAR
+DefaultAssay(tcell) <- 'chromvar'
+
+### Save
+
+FeaturePlot(
+  object = tcell,
+  features = "MA0511.2", ### Runx2
+  min.cutoff = 'q10',
+  max.cutoff = 'q90',
+  pt.size = 0.1,
+  order = T
+)
