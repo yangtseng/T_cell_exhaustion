3f7fff380eb406e41a0ed89aa935517ec41d2d39
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-08 13:35:15 +0800

    Update Fig_1e.R

--- Section_4_Figure_code/Fig_1e.R
+++ Section_4_Figure_code/Fig_1e.R
@@ -13,3 +13,20 @@ source("requirements.R")
 ### Load latest file
 HCC.tcell <- readRDS(paste0(work_path, "murine_tcell_Runx28.rds"))
 
+### Please refer to Section_1 Step_3_Module_score for related analysis code
+
+####################################################
+### Step 1, Generate the dataset for ridge plots ###
+####################################################
+
+rdp_exh <- cbind(as.data.frame(as.factor(HCC.tcell$seurat_clusters)), as.data.frame(HCC.tcell$exhaust1), as.data.frame(HCC.tcell$cp1),
+                 as.data.frame(HCC.tcell$eff1), as.data.frame(HCC.tcell$cell_type))
+
+### Modify the names in the data frame
+colnames(rdp_exh) <- c('y','exh','cp','eff', 'group')
+rdp_exh$group <- sub('Effector T cell','Effector\n T cell', rdp_exh$group)
+rdp_exh$group <- sub('Exhausted T cell','Exhausted\n T cell', rdp_exh$group)
+rdp_exh$group <- sub('Memory T cell','Memory\n T cell', rdp_exh$group)
+rdp_exh$group <- sub('Proliferative T cell','Proliferative\n T cell', rdp_exh$group)
+rdp_exh$group <- sub('High IFN response T cell','High IFN\n response\n T cell', rdp_exh$group)
+rdp_exh$y <- paste0('C', rdp_exh$y)
