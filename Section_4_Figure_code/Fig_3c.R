dc619de14b98bd26fa8d79ee38ecd360cd5d3246
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-08 16:00:10 +0800

    Update Fig_3c.R

--- Section_4_Figure_code/Fig_3c.R
+++ Section_4_Figure_code/Fig_3c.R
@@ -1,1 +1,17 @@
+########################################
+### Section 4, Code for main figures ###
+########################################
 
+#################
+### Figure 3c ###
+#################
+
+set.seed(1234)
+work_path = "./main_figures/"
+source("requirements.R")
+
+### Load latest file
+gs.go.kegg <- escape::getGeneSets(species = 'Mus musculus', library = 'C2', subcategory = 'KEGG')
+
+### Filter the KEGG pathway based on the result of pathway enrichment 
+KEGG <- gs.go.kegg[names(gs.go.kegg) %in% gsub("-", "_", rownames(KEGG_AVG27))]
