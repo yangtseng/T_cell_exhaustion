afb794e20bf3276b241e531f4decb2c1e125b4e5
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-08 16:21:13 +0800

    Update Fig_4b.R

--- Section_4_Figure_code/Fig_4b.R
+++ Section_4_Figure_code/Fig_4b.R
@@ -1,1 +1,14 @@
+########################################
+### Section 4, Code for main figures ###
+########################################
 
+#################
+### Figure 4b ###
+#################
+
+set.seed(1234)
+work_path = "./main_figures/"
+source("requirements.R")
+
+### Load latest file
+PE <- read.csv(paste0(work_path, "Pathway_enrichment_Tex.csv"), header = 1)
