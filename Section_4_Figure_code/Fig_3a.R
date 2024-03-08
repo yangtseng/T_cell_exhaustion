fbecae7f08edd9aa53f98ed283b13a3909761952
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-08 15:13:24 +0800

    Update Fig_3a.R

--- Section_4_Figure_code/Fig_3a.R
+++ Section_4_Figure_code/Fig_3a.R
@@ -1,1 +1,15 @@
+########################################
+### Section 4, Code for main figures ###
+########################################
 
+#################
+### Figure 3a ###
+#################
+
+set.seed(1234)
+work_path = "./main_figures/"
+source("requirements.R")
+
+### Load latest file
+HCC.tcell <- readRDS("./murine_tcell_Runx28.rds")
+tcell <- readRDS("./Cellline/cellline_multiome.rds")
