3afbdb97191b6184be4f806766fef51524962656
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-08 13:50:31 +0800

    Update Fig_1e.R

--- Section_4_Figure_code/Fig_1e.R
+++ Section_4_Figure_code/Fig_1e.R
@@ -11,7 +11,7 @@ work_path = "./main_figures/"
 source("requirements.R")
 
 ### Load latest file
-HCC.tcell <- readRDS(paste0(work_path, "murine_tcell_Runx28.rds"))
+HCC.tcell <- readRDS("./murine_tcell_Runx28.rds")
 
 ### Please refer to Section_1 Step_3_Module_score for related analysis code
 
