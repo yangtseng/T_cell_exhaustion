895b76f9cf7be7be46e97e03abb6e2f720c3edac
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-08 14:14:38 +0800

    Update Step_3_Basic_analysis.R

--- Section_2_Analysis_of_cell_line_like_model/Step_3_Basic_analysis.R
+++ Section_2_Analysis_of_cell_line_like_model/Step_3_Basic_analysis.R
@@ -54,12 +54,30 @@ tcell <- ScaleData(tcell)
 
 ### Identifying the differentially expressed genes (DEGs)
 tcell_DEGs <- FindAllMarkers(tcell, assay = 'SCT', logfc.threshold = 0.4, only.pos = T)
+tcell_DEGs <- tcell_DEGs[unique(tcell_DEGs$gene),]
+tcell_DEGs$cluster <- factor(tcell_DEGs$cluster, levels = c('Exhausted_T_96hr',"Exhausted_T_72hr", "Active_T","Naive_T"))
+tcell_DEGs <- tcell_DEGs[order(tcell_DEGs$cluster, tcell_DEGs$avg_log2FC, decreasing = T),]
 
 ### Identifying the differentially activated genes (based on gene activity)
 tcell_DAGs <- FindAllMarkers(tcell, assay = 'GA', logfc.threshold = 0.3, only.pos = T)
+tcell_DAGs <- tcell_DAGs[unique(tcell_DAGs$gene),]
+tcell_DAGs$cluster <- factor(tcell_DAGs$cluster, levels = c('Exhausted_T_96hr',"Exhausted_T_72hr", "Active_T","Naive_T"))
+tcell_DAGs <- tcell_DAGs[order(tcell_DAGs$cluster, tcell_DAGs$avg_log2FC, decreasing = T),]
+
+### Preparing dataframe for main figure [Fig. 2b]
+counts <- GetAssayData(tcell, assay="SCT", slot="data")
+counts <- as.matrix(counts[rownames(counts) %in% genes, ])
+
+GA_counts <- GetAssayData(tcell, assay="gene_activity", slot="data")
+GA_counts <- as.matrix(GA_counts[rownames(GA_counts) %in% tcell_DAGs$gene, ])
+
+### Scaling the matrix
+counts_scaled = t(scale(t(counts)))
+GA_counts_scaled = t(scale(t(GA_counts)))
 
 ### save files
 write.csv(tcell_DEGs, paste0(work_path, "cell_line_like_DEGs.csv"))
 write.csv(tcell_DAGs, paste0(work_path, "cell_line_like_DAGs.csv"))
 
 saveRDS(tcell, paste0(work_path, "Cellline_multiome2.rds")
+save(counts_scaled, GA_counts_scaled, file = paste0(work_path, "Cellline_DEGs.RData"))
