ead4da7e15820da283c2424795b455551d5e3a64
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-08 09:35:33 +0800

    Update Fig_1c.R

--- Section_4_Figure_code/Fig_1c.R
+++ Section_4_Figure_code/Fig_1c.R
@@ -1,1 +1,14 @@
+########################################
+### Section 4, Code for main figures ###
+########################################
 
+#################
+### Figure 1c ###
+#################
+
+set.seed(1234)
+work_path = "./external_validation/"
+source("requirements.R")
+
+### Load data
+Load(paste0(work_path, "Human_ST_bayesspace.RData"))
