1ae665403feb1a4c31cfabad750c7bdeca64b819
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-09 17:31:52 +0800

    Create Fig_5d.R

---
+++ Section_4_Figure_code/Fig_5d.R
@@ -0,0 +1,9 @@
+
+
+### The ratio of RUNX2+ exhausted T cell / CD8+ T cell in specific carcinoma region were then calculated
+### 46.34 (P1T), 35 (P3T), 25.16 (P5T), 67.36 (P8T), 77.04 (P11T) [Non-responders
+### 23.46 (P7T), 15 (P9T), 23.1 (P10T) [Responders]
+
+### load files
+load("./external_validation/Human_ST_subT.RData")
+### Including P1T.subT, P3T.subT, P5T.subT, P7T.subT, P8T.subT, P9T.subT, P10T.subT and P11T.subT
