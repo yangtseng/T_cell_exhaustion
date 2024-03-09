fb4df1a516398f0081c45f3202cdf1d82ebacc83
yangtseng <50098316+yangtseng@users.noreply.github.com>
2024-03-09 17:28:48 +0800

    Update Fig_5c.R

--- Section_4_Figure_code/Fig_5c.R
+++ Section_4_Figure_code/Fig_5c.R
@@ -1,3 +1,15 @@
+########################################
+### Section 4, Code for main figures ###
+########################################
 
+#################
+### Figure 5b ###
+#################
 
+set.seed(1234)
+work_path = "./main_figures/"
+source("requirements.R")
+
+### Load latest file
 load("./external_validation/Human_bulk.RData"))
+### Including 
