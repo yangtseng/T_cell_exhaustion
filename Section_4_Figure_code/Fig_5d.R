e1dffc134362d5c0b1573027f3776be7aab5e7c3
Tzu-Yang Tseng <50098316+yangtseng@users.noreply.github.com>
2024-05-09 14:52:40 +0800

    Update Fig_5d.R

--- Section_4_Figure_code/Fig_5d.R
+++ Section_4_Figure_code/Fig_5d.R
@@ -20,13 +20,25 @@ ST.df <- data.frame(data1 = c(46.34, 35, 25.16, 67.36, 77.04, 23.46, 15, 23.1),
 
 colnames(ST.df) <- c('data','group')
 
-p <- ggplot(ST.df, aes(x=factor(group, levels = c('Response','Non-Response')), y=data)) + 
-  geom_boxplot(fill = c('#A8D0C7','#D78D82'), outlier.shape = NA) + geom_point(position=position_jitter(width=0.03)) + 
-  geom_signif(comparisons = list(c("Response", "Non-Response")), test = t.test, test.args = c('less'), textsize = 6) + 
+p <- ggplot(ST.df, aes(x=factor(group, levels = c('Responder','Non-Responder')), y=data)) + 
+  geom_boxplot(fill = c('#A8D0C7','#D78D82'), outlier.shape = NA, size = .8) + 
+  geom_point(aes(fill = group), pch = 21, stroke = 1.5,
+             position=position_dodge2(width=0.3), size = 3.5) + 
+  scale_fill_manual(values = c('#D78D82', '#A8D0C7')) +
+  geom_signif(comparisons = list(c("Responder", "Non-Responder")), 
+              annotations = "*", textsize = 10, size = 0.8, vjust = 0.5, 
+              y_position = 80) +
   theme_bw() + ylab('Proportion of RUNX2+\nExhausted T cells (%)') + xlab('HCC Patient') + ylim(10, 85) +
-  theme(axis.text.x = element_text(size = 20), axis.text.y = element_text(size = 18),
-        axis.title.x = element_text(size = 20), axis.title.y = element_text(size = 20),
-        legend.title = element_text(size = 16), legend.text = element_text(size = 15))
+  theme(text = element_text(family = 'Arial'),
+        axis.text.x = element_text(size = 20, color = 'black', vjust = 1), 
+        axis.text.y = element_text(size = 18, color = 'black'),
+        axis.title.x = element_text(size = 20, vjust = -1), 
+        axis.title.y = element_text(size = 20),
+        panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        legend.title = element_text(size = 16), legend.text = element_text(size = 15),
+        legend.position = "None") +
+  coord_cartesian(ylim=c(16,85)) + 
+  scale_y_continuous(breaks = c(20,40,60,80))
 
 ### save
 png(
